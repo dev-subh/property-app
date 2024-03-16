59b500ff63b4bc3a892ed2b0dd2f7dba43644d74
dev-subh <dev.subhashk@gmail.com>
2024-03-16 13:15:59 +0530

    updated user registration form

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -18,7 +18,34 @@ export class UserRegisterComponent implements OnInit {
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
       confirmPassword: new FormControl(null, Validators.required),
       mobile: new FormControl(null, [Validators.required,Validators.maxLength(10)])
-    })
+    },{ validators: this.passwordMatchingValidator as ValidatorFn })
+  }
+
+  
+ 
+  passwordMatchingValidator(fg: FormGroup): ValidationErrors | null {
+    const password = fg.get('password')?.value;
+    const confirmPassword = fg.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notmatched: true };
+  }
+
+
+  // getter method for all form control
+
+  get userName(){
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get email(){
+    return this.registrationForm.get('email') as FormControl;
+  }
+  get password(){
+    return this.registrationForm.get('password') as FormControl;
+  }
+  get confirmPassword(){
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile(){
+    return this.registrationForm.get('mobile') as FormControl;
   }
 
   onSubmit(){
